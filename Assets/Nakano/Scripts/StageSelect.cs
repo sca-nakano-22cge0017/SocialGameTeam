170fbb235ea48a1c6caf2d17763cd10abc5f9004
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2024-09-26 14:23:47 +0900

    ウィンドウ表示処理作成

---
+++ Assets/Nakano/Scripts/StageSelect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelect : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
