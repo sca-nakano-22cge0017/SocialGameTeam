22ee8bb1c87f2ac394802bf5ce1b6a0efbd546b7
minegishi <>
2024-10-05 18:15:09 +0900

    プレイヤーclass作成中

---
+++ Assets/Minegishi/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    class player
+    {
+        public float physicalStrength;
+        public float defense;
+        public float attack;
+        public float magicPawer;
+        public float speed;
+        public float dexterity;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
