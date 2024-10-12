d7cfc95eb90a557b7db65944875490eb770322c2
minegishi <>
2024-10-12 17:57:26 +0900

    記録画面、UI追加

--- Assets/Minegishi/Scripts/RecordManager.cs
+++ Assets/Minegishi/Scripts/RecordManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class RecordManager : MonoBehaviour
 {
+    [SerializeField] GameObject saveWindow1;
+    [SerializeField] GameObject saveWindow2;
+    [SerializeField] GameObject saveWindow3;
+    [SerializeField] GameObject saveWindow4;
+
+    bool WindowClick = false;
+
     void Start()
     {
         
@@ -12,11 +19,39 @@ public class RecordManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (WindowClick)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                saveWindow3.SetActive(true);
+            }
+        }
     }
 
     public void BuckButton()
     {
         SceneManager.LoadScene("HomeScene");
+    }
+
+    public void NoButton()
+    {
+        saveWindow1.SetActive(false);
+        saveWindow2.SetActive(false);
+        saveWindow3.SetActive(false);
+        saveWindow4.SetActive(false);
+    }
+
+    public void YesButton1()
+    {
+        saveWindow2.SetActive(true);
+        WindowClick = true;
+    }
+
+    public void YesButton2()
+    {
+        saveWindow1.SetActive(false);
+        saveWindow2.SetActive(false);
+        saveWindow3.SetActive(false);
+        saveWindow4.SetActive(false);
     }
 }
