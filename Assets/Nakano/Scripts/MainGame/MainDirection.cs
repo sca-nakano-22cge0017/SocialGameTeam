e542fb29e6f2caa186009006dcac6286cb24dcdf
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2024-12-13 18:27:16 +0900

    ボス戦開始演出作成

--- Assets/Nakano/Scripts/MainGame/MainDirection.cs
+++ Assets/Nakano/Scripts/MainGame/MainDirection.cs
@@ -7,8 +7,7 @@ public class MainDirection : MonoBehaviour
 {
     [SerializeField] private Canvas commands;
 
-    [SerializeField] private GameObject elapsedTurnObj;
-    [SerializeField] private GameObject menuButtonObj;
+    [SerializeField] private GameObject upUI;
     [SerializeField] private GameObject commandsObj;
 
     [SerializeField] CinemachineVirtualCamera defaultCamera;
@@ -18,6 +17,12 @@ public class MainDirection : MonoBehaviour
     [SerializeField, Header("ボスにカメラ寄る長さ(秒)")] private float bossUpTime = 0.5f;
     bool isCompBossUp = false;
     [SerializeField, Header("UIがスライドイン完了するまでの時間(秒)")] private float UISlideInTime = 0.5f;
+    Vector3 defaultPos_UpUI;
+    Vector3 defaultPos_Commands;
+    [SerializeField] Vector3 initPos_UpUI;
+    [SerializeField] Vector3 initPos_Commands;
+    float moveSpeed_UpUI;
+    float moveSpeed_Commands;
 
     /// <summary>
     /// バトル開始時の演出が全て完了しているかどうか
@@ -36,6 +41,7 @@ public class MainDirection : MonoBehaviour
             defaultCamera.Priority = 1;
             bossUpCamera.Priority = 100;
 
+            ReadyBossStartDirection();
             StartCoroutine(BossStart());
         }
     }
@@ -52,6 +58,25 @@ public class MainDirection : MonoBehaviour
         isCompleteStartDirection = true;
     }
 
+    /// <summary>
+    /// ボス戦開始演出用意
+    /// </summary>
+    void ReadyBossStartDirection()
+    {
+        defaultPos_Commands = commandsObj.transform.localPosition;
+        defaultPos_UpUI = upUI.transform.localPosition;
+
+        commandsObj.transform.localPosition = initPos_Commands;
+        upUI.transform.localPosition = initPos_UpUI;
+
+        moveSpeed_Commands = (defaultPos_Commands.x - initPos_Commands.x) / UISlideInTime;
+        moveSpeed_UpUI = (defaultPos_UpUI.y - initPos_UpUI.y) / UISlideInTime;
+    }
+
+    /// <summary>
+    /// ボス戦開始演出
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator BossStart()
     {
         defaultCamera.Priority = 1;
@@ -61,6 +86,27 @@ public class MainDirection : MonoBehaviour
 
         bossUpCamera.Priority = 1;
         defaultCamera.Priority = 100;
+
+        yield return new WaitForSeconds(2.0f);
+
+        float t = 0;
+        while (t < UISlideInTime)
+        {
+            t += Time.deltaTime;
+
+            var cPos = commandsObj.transform.localPosition;
+            cPos.x += moveSpeed_Commands * Time.deltaTime;
+            commandsObj.transform.localPosition = cPos;
+
+            var uiPos = upUI.transform.localPosition;
+            uiPos.y += moveSpeed_UpUI * Time.deltaTime;
+            upUI.transform.localPosition = uiPos;
+
+            yield return null;
+        }
+
+        commandsObj.transform.localPosition = defaultPos_Commands;
+        upUI.transform.localPosition = defaultPos_UpUI;
 
         isCompBossUp = true;
         DirectionCompleteCheck();
