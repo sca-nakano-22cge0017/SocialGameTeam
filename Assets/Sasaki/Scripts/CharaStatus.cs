6b3387c3d6a36cade2f43bdf3f7bb9084063a242
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2025-01-18 08:41:23 +0900

    キャラのスパイン修正、フェードを一部変更

--- Assets/Sasaki/Scripts/CharaStatus.cs
+++ Assets/Sasaki/Scripts/CharaStatus.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -8,21 +9,51 @@ using UnityEngine.UI;
 public class CharaStatus : MonoBehaviour
 {
     [SerializeField,Header("育成終了ウィンドウ")] private GameObject resetWindow;
+    [SerializeField,Header("チェックウィンドウ")] private GameObject checkWindow;
     //[SerializeField,Header("効果説明ウィンドウ")] private GameObject effectWindow;
     [SerializeField,Header("効果表示のテキスト")] private Text effectText;
+    [SerializeField,Header("育成完了のfade")] private SpecialFade fade;
+    [SerializeField,Header("育成完了の表示")] private GameObject resetFadeText;
+    [SerializeField, Header("育成完了のテキスト")] private Text fadeText;
 
     string strEffect;
+    string strReset;
+
+    bool resetCheck = false;
     // Start is called before the first frame update
     void Start()
     {
+        fade.EyeOpen();
+        fade = FindObjectOfType<SpecialFade>();
+        resetFadeText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (resetWindow.activeSelf)
+        if (resetCheck) //育成完了したら
+        {
+            fadeBool();
+        }
+        else //育成完了表示を非表示
+        {
+            resetFadeText.SetActive(false);
+        }
+
+        strReset = "育成状況がリセットしました。\n";
+
+        if (resetWindow.activeSelf) //効果が付与されたら
         {
             effectDescription();
+            strReset = strReset + 
+                        "プラス値が付与されました。\n" +
+                        "対象ステータスの上限値が上昇します。";
+        }
+        //育成完了表示上でタップしたら
+        if (Input.GetMouseButtonDown(0) && resetFadeText.activeSelf) 
+        {
+            resetCheck = false;
+            ResetWindowScene("ResetHome");
         }
     }
     //リセットのボタン設定
@@ -30,17 +61,33 @@ public class CharaStatus : MonoBehaviour
     {
         switch (str)
         {
-            case "Reset": //育成完了(ホームに戻る)
+            case "Reset": //育成完了
                 PlayerDataManager.TraningReset();
                 Debug.Log("育成完了");
-                SceneLoader.LoadFade("HomeScene");
+                resetCheck = true;
+                checkWindow.SetActive(false);
+                fade.EyeClose();
+                break;
+            case "ResetHome": //ホーム画面に戻る
+                SceneLoader.Load("HomeScene");
+                //fade.EyeOpen();
                 break;
             default:
                 SceneLoader.LoadFade("TitleScene");
                 break;
         }
     }
-
+    //育成完了したら
+    private void fadeBool()
+    {
+        if (fade.IsFadeCompleted)
+        {
+            resetWindow.SetActive(false);
+            resetFadeText.SetActive(true);
+            fadeText.text = strReset.ToString();
+        }
+    }
+    //効果のテキスト表示
     private void effectDescription()
     {
         strEffect = PlayerDataManager.GetResetEffects();
