ed6badec3a8af58a71778bc7a89aaaf5789d61aa
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2025-01-25 19:06:30 +0900

    ノーマルキャラ差分完了

--- Assets/Sasaki/Scripts/CharaillustChanger.cs
+++ Assets/Sasaki/Scripts/CharaillustChanger.cs
@@ -6,16 +6,27 @@ using UnityEngine.UI;
 public class CharaillustChanger : MonoBehaviour
 {
     [SerializeField,Header("キャラクター")] private Sprite[] chara;
+    [SerializeField,Header("差分の剣士(ノーマルのみ)")] private Sprite[] charaADifference;
+    [SerializeField,Header("差分のシスター(ノーマルのみ)")] private Sprite[] charaBDifference;
     [SerializeField] private Image image = null;
 
     int charaNumSelect;
+    int differenceNum = 0;
+    bool charaLoss;
+    bool ispush = false;
 
+    CombiType type;
+
+    private void Start()
+    {
+        charaLoss = GameManager.islastBattleLose;
+    }
     //選ばれたキャラを表示
     void Update()
     {
         charaNumSelect = GameManager.SelectChara;
 
-        CombiType type = PlayerDataManager.player.GetSelectEvolutionType();
+        type = PlayerDataManager.player.GetSelectEvolutionType();
         if (charaNumSelect == 1)
         {
             switch (type)//剣士
@@ -31,6 +42,11 @@ public class CharaillustChanger : MonoBehaviour
                     break;
                 case CombiType.NORMAL:
                     image.sprite = chara[0];
+                    if (charaLoss)
+                    {
+                        image.sprite =charaADifference[0];
+                        differenceNum = 4;
+                    }
                     break;
                 default: 
                     image.sprite = chara[0]; 
@@ -51,12 +67,63 @@ public class CharaillustChanger : MonoBehaviour
                     image.sprite = chara[7];
                     break;
                 case CombiType.NORMAL:
-                    image.sprite = chara[4];
+                        image.sprite = chara[4];
+                    if (charaLoss)
+                    {
+                        image.sprite = charaBDifference[0];
+                        differenceNum = 4;
+                    }
                     break;
                 default:
                     image.sprite = chara[4];
                     break;
             }
         }
+        if (ispush)
+        {
+            charaLoss = false;
+            //剣士がノーマルの時
+            if (charaNumSelect == 1 && type == CombiType.NORMAL)
+            {
+                if (differenceNum == 1)
+                {
+                    image.sprite = charaADifference[differenceNum];
+                }
+                if (differenceNum == 2)
+                {
+                    image.sprite = charaADifference[differenceNum];
+                }
+                if (differenceNum == 3)
+                {
+                    image.sprite = charaADifference[differenceNum];
+                }
+                if (differenceNum > 3)
+                {
+                    differenceNum = 0;
+                }
+            }
+
+            //シスターがノーマルの時
+            if (charaNumSelect == 2 && type == CombiType.NORMAL)
+            {
+                if (differenceNum == 1)
+                {
+                    image.sprite = charaBDifference[differenceNum];
+                }
+                if (differenceNum == 2)
+                {
+                    image.sprite = charaBDifference[differenceNum];
+                }
+                if (differenceNum > 2)
+                {
+                    differenceNum = 0;
+                }
+            }
+        }
+    }
+    public void differenceImage()
+    {
+        ispush = true;
+        differenceNum++;
     }
 }
